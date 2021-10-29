cd3e8d1c622da9a9bd2ee19e5082120cd861ded5
Tan Diep <tandiep10101999@gmail.com>
2021-10-29 17:09:10 +0700

    Correct BaiTap2, expected result should be 141 not 142

--- Khoa_1_Chuong_5_DiepThanhTan/src/com/tandiep/BaiTap2.java
+++ Khoa_1_Chuong_5_DiepThanhTan/src/com/tandiep/BaiTap2.java
@@ -3,11 +3,11 @@ package com.tandiep;
 public class BaiTap2 {
 
     public static void main(String[] args) {
-        short n = 1;
+        short n = 0;
         short total = 0;
         while(total < 10000) {
-            total += n;
             n++;
+            total += n;
         }
         System.out.println("Số cần tìm là " + n);
     }
